34dc2a71bec819072614dff7c53431ed04eefa33
arekGuba <150472178+arekGuba@users.noreply.github.com>
2025-01-30 19:30:54 -0600

    HW1.java
    submit this one

--- HW1.java
+++ HW1.java
@@ -22,14 +22,15 @@ public class HW1 {
      * contains a single integer values.
      *
      * Methods:
-     *  - void   sortInserted(val)     - Inserts value 'val' into the linked-list in
-     *                                   sorted fashion
-     *  - void   removeElementsLT(val) - Removed values in the linked-list that are less
-     *                                   than 'val'
-     *  - void   removeElement(val)    - Removes all values in the linked list of
-     *                                   value 'val'
-     *  - String toString()            - coverts and returns the linked-lists as a string
-     *                                   delimited by brackets []
+     * - void sortInserted(val) - Inserts value 'val' into the linked-list in
+     * sorted fashion
+     * - void removeElementsLT(val) - Removed values in the linked-list that are
+     * less
+     * than 'val'
+     * - void removeElement(val) - Removes all values in the linked list of
+     * value 'val'
+     * - String toString() - coverts and returns the linked-lists as a string
+     * delimited by brackets []
      *
      */
 
@@ -38,13 +39,13 @@ public class HW1 {
             int data;
             Node next;
 
-            Node(int d)  {        // Constructor
+            Node(int d) { // Constructor
                 data = d;
                 next = null;
             }
         }
-        Node head;                // head of Linked-list
 
+        Node head; // head of Linked-list
 
         /*
          * Method sortedInsert() - this method will insert a new node to the
@@ -53,13 +54,13 @@ public class HW1 {
          * the linked-list.
          *
          */
-        public void sortedInsert ( int data ) {
+        public void sortedInsert(int data) {
             Node new_node = new Node(data);
 
             new_node.next = null;
 
             // Special case for head node.
-            if (this.head == null || head.data >= new_node.data ) {
+            if (this.head == null || head.data >= new_node.data) {
                 new_node.next = head;
                 head = new_node;
             } else {
@@ -77,7 +78,6 @@ public class HW1 {
             return;
         }
 
-
         /*
          * Method removeElementsLT() - this method removes all nodes that contain a
          * value that is less than the provided parameter 'ltValue'.
@@ -85,65 +85,68 @@ public class HW1 {
          * The method will invoke the method removeElements for each element
          * found in the linked-list that is less than thr parameter value passed.
          */
-        public void removeElementsLT ( int ltValue ) {
+        public void removeElementsLT(int ltValue) {
 
             // YOUR CODE GOES HERE
 
-            if (this.head != null){
-                Node cursor = this.head
-                if (cursor.data < ltValue){
-                    removeElement(cursor.data);
-                }      
+            if (this.head != null) {
+                Node cursor = this.head;
+                while (cursor != null) {
+                    if (cursor.data < ltValue) {
+                        removeElement(cursor.data);
+                    }
+                    cursor = cursor.next;
+                }
             }
             return;
         } // method removeElementsLT
-
 
         /*
          * Method removeElement() - this method removes all nodes that contain a
          * value equal to the value the provided parameter 'value'.
          */
-
-        public void removeElement ( int value ) {
+        public void removeElement(int value) {
             // YOUR CODE GOES HERE
             Node cursor = this.head;
+            /*
+            * first, make sure the head node's data isn't our target value
+            * if so, cursor moves to the 1st non-target node (if possible)
+            * to change which node should be the head.
+            */
+            if (cursor.data == value) {
+                while (cursor != null && cursor.data == value) {
+                    cursor = cursor.next;
+                }
+                this.head = cursor;
+            } 
+            /* 
+             * if the head is fine, inner-list loop runs
+             */
+            else {
+                if (cursor.next != null) { // check if more than 1 element exists
+                    // runs until cursor.next finds target value or end of list
+                    while (cursor.next != null && cursor.next.data != value) {
+                        cursor = cursor.next;
+                    }
 
-            /* initial "head" target loop
-            * once the head's data is guaranteed to not be equal to the target value,
-            * this will allow the method to use a second cursor in order to manipulate
-            * different nodes' next pointers
-            */
-            while (cursor != null && head.data == value){
-                head = cursor.next;
-                cursor.next = null;
-                cursor.data = null;
-                cursor = cursor.next;
-            }
-            
-            /* next loop, removing target nodes that aren't the head
-            * first, check if a head node exists, then if a node other than the current head exists
-            * afterwards, run the loop to remove nodes by relinking cursor.next and forwardCursor.next
-            */
-            if (cursor != null && cursor.next != null){
-                Node forwardCursor = cursor.next;
-                // removal loop
-                while (forwardCursor != null){
-                    if (forwardCursor.data == value){
-                        cursor.next = forwardCursor.next;
-                        forwardCursor.next = null;
-                        forwardCursor.data = null;
-                    }   
+                    if (cursor.next != null) { // end-of-list check for cursor
+                        Node forwardCursor = cursor.next;
+                        // runs until forwardCursor is at next non-target node or reaches null
+                        while (forwardCursor != null && forwardCursor.data == value) {
+                            forwardCursor = forwardCursor.next;
+                        }
+                        cursor.next = forwardCursor;
+                    }
                 }
             }
             return;
-        } //method removeElement
-
+        } // method removeElement
 
         /*
          * Method toString() - this is a helper method for printing / constructing
          * a string object from the linked-list.
          */
-        public String toString () // Method to output the LinkedList as a String
+        public String toString() // Method to output the LinkedList as a String
         {
             String output = "[";
             Node currNode = this.head;
@@ -156,9 +159,6 @@ public class HW1 {
 
     } // End class LinkedList
 
-
-
-
     /*
      * Class Stacks
      *
@@ -169,11 +169,11 @@ public class HW1 {
      * your solution.
      *
      * Methods:
-     *  - boolean isPalindrome(string)   - method returns true or false if 'string'
-     *                                     is a palindrome
-     *  - int     findlargestK(stack, k) - method accepts a stack and returns the
-     *                                     the largest index in the stack containing
-     *                                     value 'k' (stack index starts at 0)
+     * - boolean isPalindrome(string) - method returns true or false if 'string'
+     * is a palindrome
+     * - int findlargestK(stack, k) - method accepts a stack and returns the
+     * the largest index in the stack containing
+     * value 'k' (stack index starts at 0)
      *
      */
 
@@ -184,7 +184,8 @@ public class HW1 {
          * or 'false' on if the passed in parameter string is a palindrome or not.
          *
          * The routine should be case insensitive! Meaning 'RaCe cAr' is a palindrome.
-         * Moreover, spaces are ignore, so both 'race car' and 'racecar' are plaindromes.
+         * Moreover, spaces are ignore, so both 'race car' and 'racecar' are
+         * plaindromes.
          *
          * The method should utilize the provided Stack class.
          */
@@ -192,11 +193,23 @@ public class HW1 {
 
             Stack<Character> stack = new Stack<>();
             input = input.toLowerCase().replaceAll("\\s+", "");
-
             // Your CODE GOES HERE
-            return false;
-        }
 
+            // starting from (& including) the middle of input, push characters onto the stack
+            for (int i = input.length() / 2; i < input.length(); i++){
+                stack.push(input.charAt(i));
+            }
+
+            // compare characters in the 1st half of string to characters popping out of the stack
+            // if characters don't match, return false
+            for (int i = 0; i < input.length() / 2; i++){
+                if (input.charAt(i) != stack.pop()){
+                    return false;
+                }
+            }
+            
+            return true;
+        }
 
         /*
          * Method findLargestk() - This method will return the largest index
@@ -216,12 +229,27 @@ public class HW1 {
         public static int findLargestK(Stack<Integer> stack, int k) {
 
             // YOUR CODE GOES HERE
-            return -1;
+
+            Stack<Integer> temp = new Stack<>();
+            int currentIndex = 0;
+            int index = -1;
+
+            while (!stack.empty()){
+                temp.push(stack.pop());
+            }
+
+            while (!temp.empty()) {
+                if (temp.peek() == k) {
+                    index = currentIndex;
+                }
+                currentIndex++;
+                stack.push(temp.pop());
+            }
+
+            return index;
         }
 
-    }  // End class Stacks
-
-
+    } // End class Stacks
 
     /*******************************
      *
@@ -235,46 +263,44 @@ public class HW1 {
     public static int algorithmAnalysis1(int n, int m) {
         int a = 0, b = 0;
 
-        for (int i=0; i < n; i++)
-            a+= Math.random();
+        for (int i = 0; i < n; i++)
+            a += Math.random();
 
-        for (int j=0; j < m; j++)
-            b+= Math.random();
+        for (int j = 0; j < m; j++)
+            b += Math.random();
 
         /*
          * Select the correct option listed below:
-         *   1. O(N * M) time, O(1) space
-         *   2. O(N + M) time, O(N + M) space
-         *   3. O(N + M) time, O(1) space
-         *   4. O(N * M) time, O(N + M) space
+         * 1. O(N * M) time, O(1) space
+         * 2. O(N + M) time, O(N + M) space
+         * 3. O(N + M) time, O(1) space
+         * 4. O(N * M) time, O(N + M) space
          *
          * TODO: return the answer (which option is correct), in the return statement
-        */
+         */
 
         // RETURN THE CORRECT OPTION NUMBER LISTED ABOVE
-        return -1;
+        return 3;
     }
-
 
     public static int algorithmAnalysis2(int n) {
         int i, j, k = 0;
-        for (i = n/2; i <= n; i++)
-            for ( j = 2; j <= n; j = j*2 )
-                k+= n/2;
+        for (i = n / 2; i <= n; i++)
+            for (j = 2; j <= n; j = j * 2)
+                k += n / 2;
 
         /*
          * Select the correct option listed below:
-         *   1. O(N) time
-         *   2. O(N log N) time
-         *   3. O(N^2) time
-         *   4. O(N^2Log n) time
+         * 1. O(N) time
+         * 2. O(N log N) time
+         * 3. O(N^2) time
+         * 4. O(N^2Log n) time
          *
          * TODO: return the answer (which option is correct), in the return statement
          */
 
         // RETURN THE CORRECT OPTION LISTED ABOVE
-        return -1;
+        return 2;
     }
 
 }
-
